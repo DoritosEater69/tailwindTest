dc7164d335edbc5cfec2e5bee6a6845c19b30f4c
DoritosEater69 <AustrianPsnAcc@web.de>
2022-04-07 16:54:40 +0200

    INIT - First Thing

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: ['./public/**/*.html'],
+  content: [],
+  theme: {
+    extend: {
+      animation: {
+        inputs: "inputs 3s infinite",
+        headline: "headline 6s infinite",
+      },
+
+      keyframes: {
+        inputs: {
+          "0%": {
+            transform: "scale(1)",     
+            opacity: "1",
+          },
+          "33%": {
+            transform: "scale(1.1)",
+            opacity: "0.8",
+          },
+          "100%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+        },
+
+        headline: {
+          "0%": {
+            opacity: "1",
+          },
+          "33%": {
+            opacity: "0.4",
+            color: "white",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
